c0838ec11fe0adffc5aa79eb1ce1d8144268825b
suriyadi <suryadi.surya5@gmail.com>
2015-06-18 14:52:34 +0700

    Program pengecekan string pada CNF - Algoritma CYK

---
+++ Algoritma CYK - Generator/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Algoritma_CYK___Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAturan = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxString = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listViewTabel = new System.Windows.Forms.ListView();
+            this.labelJalan = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Aturan Produksi CFG";
+            // 
+            // textBoxAturan
+            // 
+            this.textBoxAturan.Font = new System.Drawing.Font("Segoe UI Symbol", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAturan.Location = new System.Drawing.Point(6, 34);
+            this.textBoxAturan.Multiline = true;
+            this.textBoxAturan.Name = "textBoxAturan";
+            this.textBoxAturan.Size = new System.Drawing.Size(265, 159);
+            this.textBoxAturan.TabIndex = 1;
+            this.textBoxAturan.Text = "S->AB|BC";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBoxString);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxAturan);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(277, 417);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(181, 394);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "By Suriyadi";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 106);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 78);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Proses";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxString
+            // 
+            this.textBoxString.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxString.Location = new System.Drawing.Point(0, 223);
+            this.textBoxString.Name = "textBoxString";
+            this.textBoxString.Size = new System.Drawing.Size(271, 29);
+            this.textBoxString.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Input string";
+            // 
+            // listViewTabel
+            // 
+            this.listViewTabel.Location = new System.Drawing.Point(295, 12);
+            this.listViewTabel.Name = "listViewTabel";
+            this.listViewTabel.Size = new System.Drawing.Size(547, 185);
+            this.listViewTabel.TabIndex = 5;
+            this.listViewTabel.UseCompatibleStateImageBehavior = false;
+            this.listViewTabel.View = System.Windows.Forms.View.Details;
+            // 
+            // labelJalan
+            // 
+            this.labelJalan.Location = new System.Drawing.Point(295, 203);
+            this.labelJalan.Multiline = true;
+            this.labelJalan.Name = "labelJalan";
+            this.labelJalan.ReadOnly = true;
+            this.labelJalan.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.labelJalan.Size = new System.Drawing.Size(547, 308);
+            this.labelJalan.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(100, 364);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(183, 22);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(854, 513);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelJalan);
+            this.Controls.Add(this.listViewTabel);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(870, 547);
+            this.Name = "Form1";
+            this.Text = "CYK - Check string CFG";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAturan;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxString;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listViewTabel;
+        private System.Windows.Forms.TextBox labelJalan;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Algoritma CYK - Generator/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algoritma_CYK___Generator
+{
+    public partial class Form1 : Form
+    {
+        ListView dataListview;
+        TextBox dataJalan;
+        string value;
+        public Form1()
+        {
+            InitializeComponent();
+            value = "";
+            textBoxAturan.Text = @"S->AB|BC
+A->BA|a
+B->CC|b
+C->AB|a";
+            dataListview = new ListView();
+            dataJalan = new TextBox();
+
+
+            textBoxString.Text = "aabab";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxAturan.Enabled = textBoxString.Enabled = button1.Enabled = button2.Enabled = false;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = textBoxString.Text.Length - 1;
+
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.RunWorkerAsync();
+
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataListview.Items.Clear();
+            dataListview.Columns.Clear();
+            dataJalan.Text = "";
+
+            progressBar1.Value = 0;
+
+            labelJalan.Text = "";
+            listViewTabel.Items.Clear();
+            listViewTabel.Columns.Clear();
+
+            textBoxAturan.Enabled = textBoxString.Enabled = button1.Enabled = true;
+        }
+
+        public void proses()
+        {
+            foreach (char i in textBoxString.Text)
+                dataListview.Columns.Add(i.ToString());
+
+            char[] inputString = textBoxString.Text.ToCharArray();
+
+            string[] tmpAturan = textBoxAturan.Text.Split('\n');
+            for (int i = 0; i < tmpAturan.Length; i++)
+            {
+                tmpAturan[i] = tmpAturan[i].Trim();
+            }
+            ArrayList aturan = new ArrayList();
+            for (int i = 0; i < tmpAturan.Length; i++)
+            {
+                List<string> tmp = new List<string>();
+                tmp.Add(tmpAturan[i][0].ToString());
+                string[] tmpStr = tmpAturan[i].Substring(3).Split('|');
+                foreach (string j in tmpStr)
+                    tmp.Add(j);
+                aturan.Add(tmp);
+            }
+
+            /////////////Listview baris pertama
+            for (int i = 0; i < textBoxString.Text.Length; i++)
+            {
+                string nonTerminal = "";
+                foreach (List<string> j in aturan)
+                {
+
+                    for (int k = 1; k < j.Count; k++)
+                    {
+                        if (j[k] == inputString[i].ToString())
+                        {
+                            nonTerminal += j[0];
+                            break;
+                        }
+                    }
+                }
+                if (i == 0)
+                {
+                    if (nonTerminal == "")
+                        dataListview.Items.Add("ε");
+                    else
+                        dataListview.Items.Add(nonTerminal);
+                }
+                else
+                {
+                    if (nonTerminal == "")
+                        dataListview.Items[dataListview.Items.Count - 1].SubItems.Add("ε");
+                    else
+                        dataListview.Items[dataListview.Items.Count - 1].SubItems.Add(nonTerminal);
+                }
+            }
+
+            for (int i = 1; i < inputString.Length; i++) //Isi ecek" nilai di listview
+            {
+                dataListview.Items.Add("");
+                for (int j = 1; j < inputString.Length - i; j++)
+                    dataListview.Items[i].SubItems.Add("");
+            }
+
+            ////Algoritma CYK
+            for (int j = 1; j < inputString.Length; j++)
+            {
+                backgroundWorker1.ReportProgress(j);
+
+                dataJalan.Text += string.Format("Untuk j = {0}\r\n", j + 1); //jalan
+                for (int i = 0; i < inputString.Length - 1 - j + 1; i++)
+                {
+                    dataJalan.Text += string.Format("\tUntuk i = {0}\r\n", i + 1); //jalan
+                    dataListview.Items[j].SubItems[i].Text = "ε";
+                    string hasilNonTerminal = "";
+                    for (int k = 0; k < j; k++)
+                    {
+                        dataJalan.Text += string.Format("\t\tk = {0}, i = {1}, j = {2}\r\n", k + 1,i+1,j+1); //jalan
+                        string Vik = dataListview.Items[k].SubItems[i].Text;
+                        string VikJK = dataListview.Items[j - k - 1].SubItems[i + k + 1].Text;
+
+                        //dataJalan.Text += string.Format("\t\t\tV{0}{1} = V{2}{3} ⋃ V{4}{5} ", i + 1, j + 1, i + 1, k + 1, i + 1 + k + 1, j + 1 - k - 1); //jalan
+                        char[] Ci1 = (i + 1).ToString().ToArray();
+                        string Si1 = "";
+                        foreach(char c in Ci1)
+                            Si1+=(char)int.Parse(string.Format("208{0}",int.Parse(c.ToString())), System.Globalization.NumberStyles.HexNumber);
+                        //int i1 = int.Parse(string.Format("208{0}", i + 1), System.Globalization.NumberStyles.HexNumber);
+
+                        char[] Cj1 = (j + 1).ToString().ToArray();
+                        string Sj1 = "";
+                        foreach (char c in Cj1)
+                            Sj1 += (char)int.Parse(string.Format("208{0}", int.Parse(c.ToString())), System.Globalization.NumberStyles.HexNumber);
+
+                        char[] Ck1 = (k + 1).ToString().ToArray();
+                        string Sk1 = "";
+                        foreach (char c in Ck1)
+                            Sk1 += (char)int.Parse(string.Format("208{0}", int.Parse(c.ToString())), System.Globalization.NumberStyles.HexNumber);
+
+                        char[] Ci1k1 = (i + 1 + k + 1).ToString().ToArray();
+                        string Si1k1 = "";
+                        foreach (char c in Ci1k1)
+                            Si1k1 += (char)int.Parse(string.Format("208{0}", int.Parse(c.ToString())), System.Globalization.NumberStyles.HexNumber);
+
+                        char[] Cj1k1 = (j + 1 - k - 1).ToString().ToArray();
+                        string Sj1k1 = "";
+                        foreach (char c in Cj1k1)
+                            Sj1k1 += (char)int.Parse(string.Format("208{0}", int.Parse(c.ToString())), System.Globalization.NumberStyles.HexNumber);
+                        //int j1 = int.Parse(string.Format("208{0}", j + 1), System.Globalization.NumberStyles.HexNumber);
+                        //int k1 = int.Parse(string.Format("208{0}", k + 1), System.Globalization.NumberStyles.HexNumber);
+                        //int i1k1 = int.Parse(string.Format("208{0}", i + 1 + k + 1), System.Globalization.NumberStyles.HexNumber);
+                        //int j1k1=int.Parse(string.Format("208{0}", j + 1 - k - 1), System.Globalization.NumberStyles.HexNumber);
+
+                        //dataJalan.Text += string.Format("\t\t\tV{0}{1} = Vi,k ⋃ Vi+k,j-k = V{2}{3} ⋃ V{4}{5} ", (char)i1, (char)j1, (char)i1, (char)k1, (char)i1k1, (char)j1k1); //jalan
+                        dataJalan.Text += string.Format("\t\t\tV{0},{1}\t = Vi,k ⋃ Vi+k,j-k\r\n\t\t\t\t = V{2},{3} ⋃ V{4},{5}\r\n\t\t\t\t ", Si1, Sj1, Si1, Sk1, Si1k1, Sj1k1); //jalan
+                        dataJalan.Text += string.Format("= {0} ⋃ {1}\r\n\t\t\t\t = ", Vik, VikJK); //jalan
+
+                        List<string> Union = new List<string>();
+
+                        if (Vik == "ε" && VikJK != "ε")
+                        {
+                            foreach (char v in VikJK)
+                            {
+                                Union.Add(v.ToString());
+                                dataJalan.Text += string.Format(" {0} ", v.ToString());
+                            }
+                        }
+                        else if (VikJK == "ε" && Vik != "ε")
+                        {
+                            foreach(char v in Vik)
+                            {
+                                Union.Add(v.ToString());
+                                dataJalan.Text += string.Format(" {0} ", v.ToString());
+                            }
+                        }
+                        else if (Vik != "ε" && VikJK != "ε")
+                        {
+                            foreach (char ik in Vik)
+                                foreach (char ikJK in VikJK)
+                                {
+                                    Union.Add(String.Concat(ik, ikJK));
+                                    dataJalan.Text += string.Format(" {0} ", String.Concat(ik, ikJK));
+                                }
+                        }
+                        dataJalan.Text += "\r\n";
+                        if (Vik != "ε" || VikJK != "ε")
+                        {
+                            foreach (List<string> a in aturan) //Cari Hasil dari Unnion
+                            {
+                                for (int non = 1; non < a.Count; non++)
+                                {
+                                    foreach (string u in Union)
+                                    {
+                                        if (u == a[non])
+                                        {
+                                            if (hasilNonTerminal.IndexOf(char.Parse(a[0])) == -1)
+                                                hasilNonTerminal += a[0];
+                                            break;
+                                        }
+                                    }
+                                }
+                            }////////////////////End
+                        }
+                    }
+                    if (hasilNonTerminal == "")
+                        dataListview.Items[j].SubItems[i].Text = "ε";
+                    else
+                        dataListview.Items[j].SubItems[i].Text = hasilNonTerminal;
+                }
+                dataJalan.Text += "\r\n\r\n";
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            listViewTabel.Width = this.Width - 323;
+            labelJalan.Width = this.Width - 323;
+            labelJalan.Height = this.Height - 239;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            proses();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+            foreach (ColumnHeader i in dataListview.Columns)
+                listViewTabel.Columns.Add(i.Text);
+
+            for (int i=0;i<dataListview.Items.Count;i++)
+            {
+                listViewTabel.Items.Add(dataListview.Items[i].Text);
+                for (int j = 1; j < dataListview.Items[i].SubItems.Count; j++)
+                    listViewTabel.Items[i].SubItems.Add(dataListview.Items[i].SubItems[j].Text);
+            }
+
+            labelJalan.Text = dataJalan.Text;
+
+            if (dataListview.Items[dataListview.Items.Count - 1].Text.IndexOf(textBoxAturan.Text[0]) != -1)
+                MessageBox.Show(this, "String DITERIMA...!!!", "Status", MessageBoxButtons.OK);
+            else
+                MessageBox.Show(this, "String Tidak DITERIMA...!!!", "Status", MessageBoxButtons.OK);
+        }
+    }
+}
